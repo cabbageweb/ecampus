5ce0935cc5e60d1306296113ae9682753c075f4e
Siam Ahnaf <siamahnaf198@gmail.com>
2023-09-15 20:47:46 +0600

    Adding notices

--- src/Components/Notice/Card.tsx
+++ src/Components/Notice/Card.tsx
@@ -3,34 +3,37 @@ import moment from "moment";
 import { Icon } from "@iconify/react";
 import Link from "next/link";
 
-//Urql
-import { useQuery } from "urql";
-import { GET_NOTICE_LIST } from "@/Urql/Query/Notice/notice.query";
-import { GetNoticeData } from "@/Urql/Types/Notice/notice.types";
+//Demo Data
+import { data } from "./data";
+
+// //Urql
+// import { useQuery } from "urql";
+// import { GET_NOTICE_LIST } from "@/Urql/Query/Notice/notice.query";
+// import { GetNoticeData } from "@/Urql/Types/Notice/notice.types";
 
 //Context
 import { NoticeContext } from "@/Context/notice.context";
 
 const Card = () => {
-    //Urql
-    const [{ data, fetching, error }] = useQuery<GetNoticeData>({ query: GET_NOTICE_LIST, variables: { searchInput: { search: "" } } });
+    // //Urql
+    // const [{ data, fetching, error }] = useQuery<GetNoticeData>({ query: GET_NOTICE_LIST, variables: { searchInput: { search: "" } } });
 
     //Context
     const { index } = useContext(NoticeContext);
 
-    if (fetching) return <div className="w-full h-[200px]">
-        <div className="animate-pulse h-full">
-            <div className="w-full bg-gray-300 h-full rounded-md ">
-            </div>
-        </div>
-    </div>
+    // if (fetching) return <div className="w-full h-[200px]">
+    //     <div className="animate-pulse h-full">
+    //         <div className="w-full bg-gray-300 h-full rounded-md ">
+    //         </div>
+    //     </div>
+    // </div>
 
-    if (error) return (
-        <div className="p-4">
-            <p className="text-red-600 text-center">{error.message}</p>
-        </div>
-    )
-    const notice = data?.getNotices[index]
+    // if (error) return (
+    //     <div className="p-4">
+    //         <p className="text-red-600 text-center">{error.message}</p>
+    //     </div>
+    // )
+    const notice = data?.[index]
     return (
         <div className="border-2 border-solid border-main p-3 rounded-md">
             <div className="p-5 bg-main rounded-md text-white">
